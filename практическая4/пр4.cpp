e95bcf43775002d2f5216c52447c6b5a3a47eed8
NatashaSemenenko <natata18022008@gmail.com>
2025-12-12 10:20:05 +0300

    Add files via upload

--- практическая4/пр4.cpp
+++ практическая4/пр4.cpp
@@ -1,75 +1,87 @@
 #include <iostream>
-#include <string>
 #include <windows.h>
-using namespace std;
+#include <string>
+#include <vector>
 
-const char* REG_PATH = "Software\\MyCityApp";
+const char* REG_PATH = "Software\\MyFavoriteCityApp";
 
-void SaveStringToRegistry(const string& valueName, const string& data) {
+void SaveToRegistry(const std::string& city, DWORD color) {
     HKEY hKey;
     if (RegCreateKeyExA(HKEY_CURRENT_USER, REG_PATH, 0, NULL, REG_OPTION_NON_VOLATILE, KEY_WRITE, NULL, &hKey, NULL) == ERROR_SUCCESS) {
-        RegSetValueExA(hKey, valueName.c_str(), 0, REG_SZ, (const BYTE*)data.c_str(), data.length() + 1);
+        RegSetValueExA(hKey, "FavoriteCity", 0, REG_SZ, (const BYTE*)city.c_str(), (DWORD)(city.length() + 1));
+        RegSetValueExA(hKey, "ConsoleColor", 0, REG_DWORD, (const BYTE*)&color, sizeof(DWORD));
         RegCloseKey(hKey);
+        std::cout << "Данные сохранены в реестр.\n";
+    }
+    else {
+        std::cerr << "Ошибка при открытии реестра для записи.\n";
     }
 }
 
-void SaveIntToRegistry(const string& valueName, DWORD data) {
+bool LoadFromRegistry(std::string& city, DWORD& color) {
     HKEY hKey;
-    if (RegCreateKeyExA(HKEY_CURRENT_USER, REG_PATH, 0, NULL, REG_OPTION_NON_VOLATILE, KEY_WRITE, NULL, &hKey, NULL) == ERROR_SUCCESS) {
-        RegSetValueExA(hKey, valueName.c_str(), 0, REG_DWORD, (const BYTE*)&data, sizeof(data));
+    if (RegOpenKeyExA(HKEY_CURRENT_USER, REG_PATH, 0, KEY_READ, &hKey) == ERROR_SUCCESS) {
+        char buffer[255];
+        DWORD bufferSize = sizeof(buffer);
+        DWORD colorSize = sizeof(DWORD);
+
+        if (RegQueryValueExA(hKey, "FavoriteCity", NULL, NULL, (LPBYTE)buffer, &bufferSize) == ERROR_SUCCESS) {
+            city = buffer;
+        }
+        else {
+            city = "Неизвестно";
+        }
+
+        if (RegQueryValueExA(hKey, "ConsoleColor", NULL, NULL, (LPBYTE)&color, &colorSize) != ERROR_SUCCESS) {
+            color = 7; // По умолчанию стандартный
+        }
         RegCloseKey(hKey);
+        return true;
     }
+    return false;
 }
 
-string ReadStringFromRegistry(const string& valueName) {
-    char buffer[255];
-    DWORD bufferSize = sizeof(buffer);
-    if (RegGetValueA(HKEY_CURRENT_USER, REG_PATH, valueName.c_str(), RRF_RT_REG_SZ, NULL, buffer, &bufferSize) == ERROR_SUCCESS) {
-        return string(buffer);
-    }
-    return "";
-}
-
-int ReadIntFromRegistry(const string& valueName) {
-    DWORD data = 0;
-    DWORD dataSize = sizeof(data);
-    if (RegGetValueA(HKEY_CURRENT_USER, REG_PATH, valueName.c_str(), RRF_RT_REG_DWORD, NULL, &data, &dataSize) == ERROR_SUCCESS) {
-        return (int)data;
-    }
-    return -1; // Возвращаем -1, если значение не найдено
+void SetColor(DWORD color) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, (WORD)color);
 }
 
 int main() {
     setlocale(LC_ALL, "Russian");
+    std::string city;
+    DWORD color = 7;
 
-    string city = ReadStringFromRegistry("FavoriteCity");
-    int color = ReadIntFromRegistry("ConsoleColor");
-
-    if (city.empty() || color == -1) {
-        cout << "Данные не найдены. Давайте их сохраним.\n";
-
-        cout << "Введите ваш любимый город: ";
-        getline(cin, city);
-
-        cout << "Выберите цвет текста (1 - Синий, 2 - Зеленый, 3 - Голубой, 4 - Красный, 5 - фиолетовый, 6 - Желтый, 7 - Белый): ";
-        cin >> color;
-
-        SaveStringToRegistry("FavoriteCity", city);
-        SaveIntToRegistry("ConsoleColor", (DWORD)color);
-
-        cout << "Данные успешно сохранены в реестр!\n";
-        cout << "Перезапустите программу, чтобы увидеть результат.\n";
+    if (LoadFromRegistry(city, color)) {
+        SetColor(color);
+        std::cout << "Введите Ваш любимый город: " << city << std::endl;
+        SetColor(7); // Сброс цвета на стандартный
     }
     else {
-        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-        SetConsoleTextAttribute(hConsole, (WORD)color);
-        cout << "Приветствуем в вашей программе!" << endl;
-        cout << "Ваш любимый город: " << city << endl;
-
-        // Сброс цвета к стандартному (белый) перед выходом
-        SetConsoleTextAttribute(hConsole, 7);
+        std::cout << "Сохраненные данные не найдены.\n";
     }
 
-    system("pause");
+    int choice;
+    do {
+        std::cout << "\n--- Меню ---\n";
+        std::cout << "1. Изменить любимый город и цвет\n";
+        std::cout << "2. Выход\n";
+        std::cout << "Выбор: ";
+        std::cin >> choice;
+
+        if (choice == 1) {
+            std::cout << "Введите название города: ";
+            std::cin.ignore();
+            std::getline(std::cin, city);
+
+            std::cout << "Выберите цвет текста (1 - Синий, 2 - Зеленый, 3 - голубой, 4 - Красный, 5 - фиолетовый, 6 - Желтый, 7 - Белый): ";
+            std::cin >> color;
+
+            SaveToRegistry(city, color);
+
+            SetColor(color);
+            std::cout << "\nДанные обновлены. Ваш любимый город: " << city << "!\n";
+            SetColor(7);
+        }
+    } while (choice != 2);
     return 0;
 }
